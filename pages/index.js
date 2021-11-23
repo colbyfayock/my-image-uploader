7a2d3cf98967466031ea42863ad81546b691d78f
Colby Fayock <fayock@gmail.com>
2021-11-22 20:40:29 -0500

    Step 2: collecting image from form and uploading to Cloudinary

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,25 @@ export default function Home() {
 
   async function handleOnSubmit(event) {
     event.preventDefault();
+
+    const form = event.currentTarget;
+    const fileInput = Array.from(form.elements).find(({ name }) => name === 'file');
+
+    const formData = new FormData();
+
+    for ( const file of fileInput.files ) {
+      formData.append('file', file);
+    }
+
+    formData.append('upload_preset', 'my-uploads');
+
+    const data = await fetch('https://api.cloudinary.com/v1_1/colbydemo/image/upload', {
+      method: 'POST',
+      body: formData
+    }).then(r => r.json());
+
+    setImageSrc(data.secure_url);
+    setUploadData(data);
   }
 
   return (
